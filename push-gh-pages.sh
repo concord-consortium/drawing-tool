02194582bd71d42e75a3eccfb808f34f4cea876c
Piotr Janik <janikpiotrek@gmail.com>
2014-06-20 15:48:55 +0200

    Add simple push-gh-pages.sh script
    [#73478504]

---
+++ push-gh-pages.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo "- running brunch build"
+brunch build
+
+echo "- cloning drawing-tool repo into temporary dir"
+git clone git@github.com:concord-consortium/drawing-tool.git --branch gh-pages __gh-pages-tmp__
+
+echo "- copying /public content into temporary dir"
+rsync -av --delete public/ __gh-pages-tmp__/ --exclude=".git/"
+cd __gh-pages-tmp__
+
+echo "- committing changes in gh-pages branch"
+git add --all .
+git commit -m "Auto-generated build commit"
+
+echo "- pushing gh-pages branch"
+git push origin gh-pages -f
+cd ..
+
+echo "- removing temporary dir"
+rm -rf __gh-pages-tmp__

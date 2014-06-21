eb4ae4a12f241107029a28d20996f471a65a0e6f
Andrew Dai <theman@andrewdai.co>
2014-06-21 00:26:16 -0400

    started with fabric.js, can now move a rectangle on screen

--- app/scripts/drawing-tool.js
+++ app/scripts/drawing-tool.js
@@ -2,6 +2,17 @@
 function DrawingTool(selector) {
   // Implement me!
   console.log("Drawing Tool created");
+  var canvas = new fabric.Canvas(selector);
+
+  var rect = new fabric.Rect({
+    left:100,
+    top: 100,
+    fill: 'red',
+    width: 20,
+    height: 20
+  });
+
+  canvas.add(rect)
 }
 
 module.exports = DrawingTool;

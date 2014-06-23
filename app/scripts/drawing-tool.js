8e0d8366aacc2b75b04963d04b4b623c92d8dbd3
Andrew Dai <theman@andrewdai.co>
2014-06-23 17:07:54 -0400

    added line button and have working inheritance... kind of

--- app/scripts/drawing-tool.js
+++ app/scripts/drawing-tool.js
@@ -1,92 +1,76 @@
 // Constructor function.
-function DrawingTool(selector) {
+function DrawingTool (selector) {
   // Implement me!
   console.log("Drawing Tool created");
-  var canvas = new fabric.Canvas(selector);
+  this.canvas = new fabric.Canvas(selector);
   fabric.Object.prototype.transparentCorners = false;
 
-  addRect(canvas,100,100,'red',20,20);
-  addCirc(canvas,300,100,'green',20,50);
-  var addRectBtn = document.getElementById("rect");
-  addRectBtn.addEventListener('click',
-  function(){addRect(canvas,200,200,"blue",100,100)},
-  false);
+  this.getCanvas = function(){ return this.canvas; }
 
-  var addCircBtn = document.getElementById("circ");
-  addCircBtn.addEventListener('click',
-  function(){addCirc(canvas,22,100,'green',20)},
-  false);
+  this.selectionTool = new SelectionTool();
+  this.lineTool = new LineTool();
+  // var rectangleTool = new RectangleTool();
+  // var ellipseTool = new EllipseTool();
+  // var squareTool = new SquareTool();
+  // var circleTool = new CircleTool();
 
-  var fancyBtn = document.getElementById("fancy");
-  fancyBtn.addEventListener('click',
-  function(){
-    console.log("click, boom.");
-    canvas.selection = false;
-    var startX = 0;
-    var startY = 0;
-    var rect = new fabric.Rect();
-    canvas.on('mouse:down', function(options){
-      console.log("mouse down");
-      startX = options.e.layerX;
-      startY = options.e.layerY;
-      rect = addRect(canvas, startX, startY, "purple",0,0);
-      canvas.on('mouse:move', function(options){
-        console.log("resizing");
-        w = options.e.layerX - startX;
-        h = options.e.layerY - startY;
-        rect.width = w;
-        rect.height = h;
-        canvas.renderAll(false);
-      })
-    })
-    canvas.on('mouse:up', function(options){
-      if (rect.width < 0){
-        rect.width = -rect.width;
-        rect.left = rect.left - rect.width;
-      }
-      if (rect.height < 0){
-        rect.height = -rect.height;
-        rect.top = rect.top - rect.height;
-      }
 
-      canvas.renderAll(false);
-      canvas.selection = true;
-      rect.selection = true;
-      canvas.off('mouse:down');
-      canvas.off('mouse:move');
-      canvas.off('mouse:up');
-      console.log("done?");
-      console.log(rect.width);
-    })
-    console.log(canvas.getObjects().length);
-  },
-  false);
-
+  this.currentTool = this.selectionTool;
+  console.log("drawing tool constructor finished");
 }
 
-// adds a new rectangle to the canvas based on provided
-// parameters:
-// l - left
-// t - top
-// f - fill color
-// w - width
-// h - height
-function addRect(c, l, t, f, w, h){
-  console.log("added a " + f + " rectangle!");
-  var rect = new fabric.Rect({
-    left: l, top: t, fill: f, width: w, height: h
-  });
-  c.add(rect);
-  return rect;
+var Tool = function Tool (selector){
+  this.name = "Tool";
+  this.active = false;
+
+  this.btn = document.getElementById(selector);
+  this.btn.addEventListener('click',function(){console.log("clicked")},false);
+  // How much of this is bad practice? (also probably unneccessary)
+  var self = this;
+  this.btn.addEventListener('click',function(){self.setActive(true)},false);
+  // this.btn.addEventListener('click',function(){this.setActive(true)},false);
 }
 
-function addCirc(c, l, t, f, r){
-  console.log("added a " + f + " circle!");
-  var circle = new fabric.Circle({
-    left: l, top: t, fill: f, radius: r
-  });
-  c.add(circle);
-  return;
+Tool.prototype.getButton = function (){ return this.btn; }
+
+Tool.prototype.setActive = function(active){
+  console.log(this.name + " active? " +  active);
+  if (this.active === active) { return active; }
+  this.active = active;
+  if (active === true){
+    // this tool is now active
+    console.log(this.name + " has been activated");
+    this.activate();
+  }
+  else{
+    // this tool has been deselected
+    console.log(this.name + " is no longer active");
+    // this.deactivate();
+  }
+
+  return active;
 }
+
+Tool.prototype.isActive = function(){ return this.active; }
+
+function SelectionTool (){
+  this.selector = "";
+  this.name = "Selection Tool";
+  console.log(this.name + " initialized");
+  this.setActive(true);
+}
+
+SelectionTool.prototype = new Tool("select");
+SelectionTool.prototype.activate = function(){ console.log(this.name + " success!")};
+
+function LineTool(){
+  this.selector = "";
+  this.name = "Line Tool";
+  console.log(this.name + " initialized");
+  this.setActive(false);
+}
+LineTool.prototype = new Tool("line");
+LineTool.prototype.activate = function(){ console.log(this.name + " success!")};
+
 
 module.exports = DrawingTool;

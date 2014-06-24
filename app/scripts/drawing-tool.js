6251c91f6424d4ef87228efb9289702584499334
Andrew Dai <theman@andrewdai.co>
2014-06-24 13:42:26 -0400

    button layout on the left side (not active) and demo rectangle on fabric

--- app/scripts/drawing-tool.js
+++ app/scripts/drawing-tool.js
@@ -7,28 +7,55 @@ function DrawingTool (selector) {
 
   this.getCanvas = function(){ return this.canvas; }
 
+  var rect3 = new fabric.Rect({
+    width: 50, height: 100, left: 275, top: 350, angle: 45,
+    stroke: '#eee', strokeWidth: 10,
+    fill: 'rgba(0,0,200,0.5)'
+  });
+  this.canvas.add(rect3);
+
+  $(document).ready(function(){
+    $('.btn').click(function(){
+      console.log($(this).find("input").val());
+    });
+  });
+
   this.selectionTool = new SelectionTool();
   this.lineTool = new LineTool();
-  // var rectangleTool = new RectangleTool();
+  // this.rectangleTool = new RectangleTool();
   // var ellipseTool = new EllipseTool();
   // var squareTool = new SquareTool();
   // var circleTool = new CircleTool();
 
+  var tools = [this.selectionTool, this.lineTool];//, this.rectangleTool];
+  for (var i = tools.length - 1; i >= 0; i--) {
+    console.log(tools[i]);
+    // tools[i].getButton().addEventListener("click", function(){console.log("hihihih")},false);
+  };
 
   this.currentTool = this.selectionTool;
   console.log("drawing tool constructor finished");
+}
+
+DrawingTool.prototype.toolManager = function(){
+  console.log(this.name);
 }
 
 var Tool = function Tool (selector){
   this.name = "Tool";
   this.active = false;
 
-  this.btn = document.getElementById(selector);
-  this.btn.addEventListener('click',function(){console.log("clicked")},false);
-  // How much of this is bad practice? (also probably unneccessary)
-  var self = this;
-  this.btn.addEventListener('click',function(){self.setActive(true)},false);
-  // this.btn.addEventListener('click',function(){this.setActive(true)},false);
+  if (document.getElementById(selector).checked){
+    console.log(this.name + " is checked");
+  }
+  else { console.log("nope"); }
+
+  // this.btn = document.getElementById(selector);
+  // this.btn.addEventListener('click',function(){console.log("clicked")},false);
+
+  // // How much of this is bad practice? (also probably unneccessary)
+  // var self = this;
+  // this.btn.addEventListener('click',function(){self.setActive(true)},false);
 }
 
 Tool.prototype.getButton = function (){ return this.btn; }
@@ -54,7 +81,6 @@ Tool.prototype.setActive = function(active){
 Tool.prototype.isActive = function(){ return this.active; }
 
 function SelectionTool (){
-  this.selector = "";
   this.name = "Selection Tool";
   console.log(this.name + " initialized");
   this.setActive(true);
@@ -64,13 +90,22 @@ SelectionTool.prototype = new Tool("select");
 SelectionTool.prototype.activate = function(){ console.log(this.name + " success!")};
 
 function LineTool(){
-  this.selector = "";
   this.name = "Line Tool";
   console.log(this.name + " initialized");
   this.setActive(false);
 }
 LineTool.prototype = new Tool("line");
 LineTool.prototype.activate = function(){ console.log(this.name + " success!")};
+
+// function RectangleTool(){
+//   this.name = "RectangleTool - Test";
+//   console.log(this.name + " initialized");
+//   this.setActive(false);
+// }
+// RectangleTool.prototype = new Tool("rect");
+// RectangleTool.prototype.activate = function(){
+//   console.log(this.name + " success!");
+// }
 
 
 module.exports = DrawingTool;

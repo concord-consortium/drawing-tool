005154a86aacc2fe3e539e4204aa7406d1ff9348
Piotr Janik <janikpiotrek@gmail.com>
2014-07-08 14:54:05 +0100

    Fix freehand tool, update version to 0.2.1

--- app/scripts/free-draw.js
+++ app/scripts/free-draw.js
@@ -1,12 +1,11 @@
-var inherit = require('scripts/inherit');
-var ShapeTool    = require('scripts/shape-tool');
+var inherit   = require('scripts/inherit');
+var ShapeTool = require('scripts/shape-tool');
 
 function FreeDrawTool(name, selector, drawTool) {
   ShapeTool.call(this, name, selector, drawTool);
 
   var self = this;
   this.addEventListener("mouse:down", function (e) { self.mouseDown(e); });
-  this.addEventListener("mouse:move", function (e) { self.mouseMove(e); });
   this.addEventListener("mouse:up", function (e) { self.mouseUp(e); });
 
   this.setLabel('F');
@@ -14,43 +13,38 @@ function FreeDrawTool(name, selector, drawTool) {
 
 inherit(FreeDrawTool, ShapeTool);
 
-FreeDrawTool.prototype.activate = function () {
-  FreeDrawTool.super.activate.call(this);
-  this.canvas.isDrawingMode = true;
+FreeDrawTool.prototype.mouseDown = function (opt) {
+  FreeDrawTool.super.mouseDown.call(this, opt);
+  if (!this.active) { return; }
+  if (!this.canvas.isDrawingMode) {
+    // If we are here, it means the handler is called for the first time.
+    // Activate drawing mode and call manually FabricJS handler to handle
+    // mouse down in drawing mode correctly.
+    //
+    // If you take look at FabricJS's methods like:
+    // - _onMouseDownInDrawingMode
+    // - _onMouseMoveInDrawingMode
+    // - _onMouseUpInDrawingMode
+    // it's visible that we could implement whole functionality using public
+    // `freeDrawingBrush` object. That would be better solution if these methods
+    // didn't handle clipping too. It would force us to literally copy the same
+    // code. So unless almost everything is handled in brush class, IMHO it's
+    // better to use this solution which is at least short and simple.
+    this.canvas.isDrawingMode = true;
+    this.canvas._onMouseDownInDrawingMode(opt.e);
+  }
 };
 
-FreeDrawTool.prototype.deactivate = function () {
-  FreeDrawTool.super.deactivate.call(this);
+FreeDrawTool.prototype.mouseUp = function (opt) {
+  FreeDrawTool.super.mouseUp.call(this, opt);
   this.canvas.isDrawingMode = false;
-};
-
-FreeDrawTool.prototype.mouseDown = function (evt) {
-  // need to find the target and add it to a new object along with the event
-  // in order to emulate the behavior of other shape tools on mouse/touch down
-  // events
-  var targetObj = this.canvas.findTarget(evt.e);
-  var modifiedEvent = {e: evt.e, target: targetObj};
-  FreeDrawTool.super.mouseDown.call(this, modifiedEvent);
-};
-
-
-// This hack doesn't really work and definitely doesn't work on touchscreens
-FreeDrawTool.prototype.mouseMove = function (e) {
-  if (!this._firstAction) {
-    this.canvas.isDrawingMode = this.canvas.findTarget(e.e) === undefined;
-    this._setAllObjectsSelectable(!this.canvas.isDrawingMode);
-    console.log(this.canvas.isDrawingMode);
-  }
-};
-
-FreeDrawTool.prototype.mouseUp = function (e) {
-  FreeDrawTool.super.mouseUp.call(this, e);
-  // if deactivation (marked with this.active) occurs during this step
-  // it can only be because of a movement that was below the threshold
+  var objects = this.canvas.getObjects();
+  var lastObject = objects[objects.length - 1];
   if (!this.active) {
-    this.canvas.remove(this.canvas.item(this.canvas.getObjects().length - 1));
+    this.canvas.remove(lastObject);
+    return;
   }
-  this.actionComplete();
+  this.actionComplete(lastObject);
 };
 
 module.exports = FreeDrawTool;

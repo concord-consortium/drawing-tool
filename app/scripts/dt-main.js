b1290f7b2e68ecf976fe74f3fdbe30665048b1a9
Andrew Dai <theman@andrewdai.co>
2014-07-18 15:30:43 -0400

    deleted dt-main (never used)

--- app/scripts/dt-main.js
+++
@@ -1,9 +0,0 @@
-var DrawingTool  = require('drawing-tool');
-var UI           = require('ui');
-
-function DTMain (selector) {
-  this.drawingTool = new DrawingTool (selector);
-  this.ui = new UI (drawingTool, selector, options);
-}
-
-module.exports = DTMain;

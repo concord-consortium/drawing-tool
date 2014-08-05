105a9c35919f5c0eb36e2ae3a13298b23749ea6d
Piotr Janik <janikpiotrek@gmail.com>
2014-08-05 11:34:44 +0100

    Implement rescale-2-resize for text
    [#73648682]

--- app/scripts/fabric-extensions/rescale-2-resize.js
+++ app/scripts/fabric-extensions/rescale-2-resize.js
@@ -16,12 +16,20 @@ var resizers = {
   },
   line: function (s) {
     basicWidthHeightTransform(s);
-
-    if (s.x1 > s.x2) { s.x1 = s.left + s.width; s.x2 = s.left; }
-    else { s.x2 = s.left + s.width; s.x1 = s.left; }
-
-    if (s.y1 > s.y2) { s.y1 = s.top + s.height; s.y2 = s.top; }
-    else { s.y2 = s.top + s.height; s.y1 = s.top; }
+    if (s.x1 > s.x2) {
+      s.x1 = s.left + s.width;
+      s.x2 = s.left;
+    } else {
+      s.x2 = s.left + s.width;
+      s.x1 = s.left;
+    }
+    if (s.y1 > s.y2) {
+      s.y1 = s.top + s.height;
+      s.y2 = s.top;
+    } else {
+      s.y2 = s.top + s.height;
+      s.y1 = s.top;
+    }
   },
   arrow: function (s) {
     this.line(s);
@@ -44,6 +52,17 @@ var resizers = {
       s.path[i][3] *= s.scaleX;
       s.path[i][4] *= s.scaleY;
     }
+  },
+  'i-text': function (s) {
+    // Note that actually there is no rescale to resize transformation.
+    // Rescaling is fine for text, we only just move scale from scaleX/Y
+    // attributes to fontSize and strokeWidth.
+    s.set({
+      fontSize: s.get('fontSize') * s.get('scaleX'),
+      strokeWidth: s.get('strokeWidth') * s.get('scaleX'),
+      scaleX: 1,
+      scaleY: 1
+    });
   }
 };
 

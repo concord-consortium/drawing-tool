396619b41bf7146e83c22bc4b5f72c2525380a8a
Piotr Janik <janikpiotrek@gmail.com>
2014-08-05 12:35:19 +0100

    Do not require central origin for multi-touch gestures
    [#73648682]
    
    Just transform it dynamically in case of need.
    It's important for text objects, as generally we don't want to set
    their origin to center (looks strange when you type and text growths
    in both directions).

--- app/scripts/fabric-extensions/multi-touch-support.js
+++ app/scripts/fabric-extensions/multi-touch-support.js
@@ -7,6 +7,9 @@ module.exports = function addMultiTouchSupport(canvas) {
 
   var initialAngle;
   var initialScale;
+  var shouldCenterOrigin;
+  var originalOriginX;
+  var originalOriginY;
 
   mc.on('pinchstart', function (e) {
     var target = getTarget();
@@ -16,6 +19,12 @@ module.exports = function addMultiTouchSupport(canvas) {
     setLocked(target, true);
     initialAngle = target.get('angle');
     initialScale = target.get('scaleX');
+    // While performing multi-touch gestures like pinch and zoom, it feels more natural
+    // when origin is in center.
+    shouldCenterOrigin = target.originX !== 'center' || target.originY !== 'center';
+    if (shouldCenterOrigin) {
+      setOriginToCenter(target);
+    }
   });
 
   mc.on('pinchmove', function (e) {
@@ -23,12 +32,15 @@ module.exports = function addMultiTouchSupport(canvas) {
     if (!target || isLine(target)) {
       return;
     }
+
     target.set({
       scaleX: e.scale * initialScale,
       scaleY: e.scale * initialScale,
       angle: initialAngle + e.rotation
     });
+
     canvas.fire('object:scaling', {target: target, e: e.srcEvent});
+    canvas.fire('object:rotating', {target: target, e: e.srcEvent});
     if (target.get('scaleX') !== e.scale * initialScale) {
       // rescale-2-resize mod used.
       initialScale = 1 / e.scale;
@@ -37,6 +49,9 @@ module.exports = function addMultiTouchSupport(canvas) {
 
   mc.on('pinchend', function (e) {
     var target = getTarget();
+    if (shouldCenterOrigin) {
+      resetOrigin(target);
+    }
     if (!target || isLine(target)) {
       return;
     }
@@ -58,5 +73,32 @@ module.exports = function addMultiTouchSupport(canvas) {
       lockScalingX: v,
       lockScalingY: v
     });
+  }
+
+  // Note that these functions are based on Fabric's _setOriginToCenter and _resetOrigin
+  // (as they are marked as private).
+  function setOriginToCenter(object) {
+    originalOriginX = object.originX;
+    originalOriginY = object.originY;
+
+    var center = object.getCenterPoint();
+
+    object.originX = 'center';
+    object.originY = 'center';
+    object.left = center.x;
+    object.top = center.y;
+  }
+
+  function resetOrigin(object) {
+    var originPoint = object.translateToOriginPoint(
+      object.getCenterPoint(),
+      originalOriginX,
+      originalOriginY);
+
+    object.originX = originalOriginX;
+    object.originY = originalOriginY;
+
+    object.left = originPoint.x;
+    object.top = originPoint.y;
   }
 };

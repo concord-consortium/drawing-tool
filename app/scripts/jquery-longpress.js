a6f6b66c60db7390dd6414391c3ddfe3b9d2435e
Piotr Janik <janikpiotrek@gmail.com>
2014-08-25 16:59:51 +0200

    Implement simple jQuery longPress plugin
    [#77436188]

---
+++ app/scripts/jquery-longpress.js
@@ -0,0 +1,15 @@
+(function ($) {
+
+  $.fn.longPress = function(listener, timeout) {
+    return this.on('mousedown touchstart', function (e) {
+      var timer;
+      timer = setTimeout(function () {
+        listener.call(this, e);
+      }, timeout || 150);
+      $(window).one('mouseup touchend touchcancel touchleave', function() {
+        clearTimeout(timer);
+      });
+    });
+  };
+
+})(jQuery);

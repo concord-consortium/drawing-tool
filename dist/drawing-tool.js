dd111d3722b3c434f570ac795b8b7129f0df59e0
Piotr Janik <janikpiotrek@gmail.com>
2014-08-29 19:42:17 +0200

    Release version 1.1.1
    - Change order of buttons.

--- dist/drawing-tool.js
+++ dist/drawing-tool.js
@@ -2674,6 +2674,26 @@ var ui = {
       }
     },
     {
+      name: 'sendToBack',
+      label: 'm',
+      classes: 'dt-send-to',
+      palette: 'main',
+      onInit: lockWhenNothingIsSelected,
+      onClick: function () {
+        this.dt.sendSelectionToBack();
+      }
+    },
+    {
+      name: 'sendToFront',
+      label: 'l',
+      classes: 'dt-send-to',
+      palette: 'main',
+      onInit: lockWhenNothingIsSelected,
+      onClick: function () {
+        this.dt.sendSelectionToFront();
+      }
+    },
+    {
       name: 'undo',
       label: 'u',
       classes: 'dt-undo-redo',
@@ -2707,26 +2727,6 @@ var ui = {
         this.dt.on("redo:impossible", function () {
           this.setLocked(true);
         }.bind(this));
-      }
-    },
-    {
-      name: 'sendToFront',
-      label: 'l',
-      classes: 'dt-send-to',
-      palette: 'main',
-      onInit: lockWhenNothingIsSelected,
-      onClick: function () {
-        this.dt.sendSelectionToFront();
-      }
-    },
-    {
-      name: 'sendToBack',
-      label: 'm',
-      classes: 'dt-send-to',
-      palette: 'main',
-      onInit: lockWhenNothingIsSelected,
-      onClick: function () {
-        this.dt.sendSelectionToBack();
       }
     },
     {

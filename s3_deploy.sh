0b6d5abaf74a7f03f876f582313b72d725eba732
Scott Cytacki <scytacki@concord.org>
2021-08-13 10:02:37 -0400

    This uses the starter project deploy scripts
    it disables the distribution invalidation code which typically causes problems
    and usually isn't necessary

--- s3_deploy.sh
+++ s3_deploy.sh
@@ -1,21 +1,44 @@
 #!/bin/bash
-SRC_DIR='dist'
-DISTRIBUTION_ID='E1QHTGVGYD1DWZ'
-RESOURCE_DIR='drawing-tool'
-# name of branch to deploy to root of site
-PRODUCTION_BRANCH='master'
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-	echo "skipping deploy to S3: this is a pull request"
-	exit 0
-fi
+# Typically this is the Project name.
+# The trailing slash is important
+# Can be set to an empty string for working at the top level of the bucket
+S3_BUCKET_PREFIX='drawing-tool/'
+# AWS CloudFront distribution ID
+DISTRIBUTION_ID='E1QHTGVGYD1DWZ'
+# AWS CloudFront distribution domain
+DISTRIBUTION_DOMAIN='models-resources.concord.org'
+# name of branch to deploy to root of site
+ROOT_BRANCH='master'
+# Bucket to deploy to, typically this is 'model-resources', but some projects
+# have their own buckets
+S3_BUCKET='models-resources'
+# location of built files
+SRC_DIR='dist'
+
+# exit when any command fails
+set -e
+
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting
+trap 'echo "\"${last_command}\" command exited with code $?."' EXIT
 
 # extract current TAG if present
 # the 2> is to prevent error messages when no match is found
-CURRENT_TAG=`git describe --tags --exact-match $TRAVIS_COMMIT 2> /dev/null`
+# the || echo prevents script exit when it doesn't match
+CURRENT_TAG=`git describe --tags --exact-match $GITHUB_SHA 2> /dev/null || echo ''`
+
+# Extract the branch or tag name from the GITHUB_REF
+# it should either be: refs/head/branch-name or
+# or refs/tags/v1.2.3
+# since we ought to know if this is a branch or tag based on the ref
+# we could simplify the CURRENT_TAG approach above
+BRANCH_OR_TAG=${GITHUB_REF#refs/*/}
+echo branch or tag: $BRANCH_OR_TAG
 
 # strip PT ID from branch name for branch builds
-DEPLOY_DIR_NAME=$TRAVIS_BRANCH
+DEPLOY_DIR_NAME=$BRANCH_OR_TAG
 PT_PREFIX_REGEX="^([0-9]{8,}-)(.+)$"
 PT_SUFFIX_REGEX="^(.+)(-[0-9]{8,})$"
 if [[ $DEPLOY_DIR_NAME =~ $PT_PREFIX_REGEX ]]; then
@@ -26,35 +49,52 @@ if [[ $DEPLOY_DIR_NAME =~ $PT_SUFFIX_REGEX ]]; then
 fi
 
 # tagged builds deploy to /version/TAG_NAME
-if [ "$TRAVIS_BRANCH" = "$CURRENT_TAG" ]; then
+if [ "$BRANCH_OR_TAG" = "$CURRENT_TAG" ]; then
   mkdir -p _site/version
-  S3_DEPLOY_DIR="version/$TRAVIS_BRANCH"
+  S3_DEPLOY_DIR="version/$BRANCH_OR_TAG"
   DEPLOY_DEST="_site/$S3_DEPLOY_DIR"
-  INVAL_PATH="/$RESOURCE_DIR/version/$TRAVIS_BRANCH/index.html"
-  # used by s3_website.yml
-  export S3_DEPLOY_DIR
+  INVAL_PATH="/version/$BRANCH_OR_TAG/index.html"
+  # in this case we are going to deploy this code to a subfolder of version
+  # So ignore everything except this folder.
+  # Currently this only escapes `.`
+  S3_DEPLOY_DIR_ESCAPED=$(sed 's/[.]/[&]/g;' <<<"$S3_DEPLOY_DIR")
+  IGNORE_ON_SERVER="^(?!$S3_BUCKET_PREFIX$S3_DEPLOY_DIR_ESCAPED/)"
 
-# production branch builds deploy to root of site
-elif [ "$TRAVIS_BRANCH" = "$PRODUCTION_BRANCH" ]; then
+# root branch builds deploy to root of site
+elif [ "$BRANCH_OR_TAG" = "$ROOT_BRANCH" ]; then
   DEPLOY_DEST="_site"
-  INVAL_PATH="/$RESOURCE_DIR/index.html"
+  INVAL_PATH="/index.html"
+  # in this case we are going to deploy this branch to the top level
+  # so we need to ignore the version and branch folders
+  IGNORE_ON_SERVER="^$S3_BUCKET_PREFIX(version/|branch/)"
 
 # branch builds deploy to /branch/BRANCH_NAME
 else
   mkdir -p _site/branch
   S3_DEPLOY_DIR="branch/$DEPLOY_DIR_NAME"
   DEPLOY_DEST="_site/$S3_DEPLOY_DIR"
-  INVAL_PATH="/$RESOURCE_DIR/branch/$DEPLOY_DIR_NAME/index.html"
-  # used by s3_website.yml
-  export S3_DEPLOY_DIR
+  INVAL_PATH="/branch/$DEPLOY_DIR_NAME/index.html"
+  # in this case we are going to deploy this code to a subfolder of branch
+  # So ignore everything except this folder.
+  # Currently this only escapes `.`
+  S3_DEPLOY_DIR_ESCAPED=$(sed 's/[.]/[&]/g;' <<<"$S3_DEPLOY_DIR")
+  IGNORE_ON_SERVER="^(?!$S3_BUCKET_PREFIX$S3_DEPLOY_DIR_ESCAPED/)"
 fi
+
+# used by s3_website.yml
+export S3_BUCKET_PREFIX
+export IGNORE_ON_SERVER
+export DISTRIBUTION_ID
+export DISTRIBUTION_DOMAIN
+export S3_BUCKET
 
 # copy files to destination
 mv $SRC_DIR $DEPLOY_DEST
 
-# deploy the site contents
-s3_website push --site _site
+# deploy the site contents; increase memory for s3_website gem
+echo Deploying "$BRANCH_OR_TAG" to "$S3_BUCKET:$S3_BUCKET_PREFIX$S3_DEPLOY_DIR"...
+JAVA_TOOL_OPTIONS="-Xms1g -Xmx2g" s3_website push --site _site
 
 # explicit CloudFront invalidation to workaround s3_website gem invalidation bug
 # with origin path (https://github.com/laurilehmijoki/s3_website/issues/207).
-aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths $INVAL_PATH
+# aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths $INVAL_PATH
